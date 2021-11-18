cb7107b1b6b8e64c1360fd548624dccb151d0f48
bgravanda <bgravanda@student.bridgew.edu>
2021-11-18 11:06:48 -0500

    Initialize

--- src/main/java/bsu/comp152/brennangravandaproject4/StoreDisplayController.java
+++ src/main/java/bsu/comp152/brennangravandaproject4/StoreDisplayController.java
@@ -1,10 +1,18 @@
 package bsu.comp152.brennangravandaproject4;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class StoreDisplayController implements Initializable {
     @FXML
@@ -12,11 +20,26 @@ public class StoreDisplayController implements Initializable {
     @FXML
     private TextField PriceField;
     @FXML
-    private TextField ItemTypeField;
+    private TextField ItemTypeChoice;
     @FXML
     private ListView<merchandiseItem> ListControl;
     private merchandiseItem List;
     public void LoadList(){   //Loads the merchandise items into an observable list
+        ObservableList<merchandiseItem> ItemList = FXCollections.observableArrayList();
+        ListControl.setItems(ItemList);
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        LoadList();
+        ListControl.getSelectionModel().selectedItemProperty().addListener
+                (new ChangeListener<merchandiseItem>() {
+                    @Override
+                    public void changed(ObservableValue<? extends merchandiseItem> observableValue, merchandiseItem merchandiseItem, merchandiseItem t1) {
+                        NameField.setText(t1.getName());
+                        PriceField.setText(t1.getPrice());
+                        ItemTypeChoice.setText(t1.getType());
+                    }
+                });
     }
 }
